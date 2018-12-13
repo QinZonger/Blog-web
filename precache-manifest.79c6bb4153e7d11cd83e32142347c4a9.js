32e59ca85f2d6dd141dccca1d5d17b591899fbe1
QinZonger <17611573068@163.com>
2018-12-13 20:50:03 +0800

    Updates

---
+++ precache-manifest.79c6bb4153e7d11cd83e32142347c4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "7c7e2531276a4b60da5a",
+    "url": "/react-app/static/js/main.7c7e2531.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-app/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "7c7e2531276a4b60da5a",
+    "url": "/react-app/static/css/main.1855e724.chunk.css"
+  },
+  {
+    "revision": "01205c01ba0dcdbb974df04745a8aec8",
+    "url": "/react-app/index.html"
+  }
+];
