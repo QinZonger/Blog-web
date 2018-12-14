b52d730026812bc2b514cd898ebb021f30c6aa6a
QinZonger <17611573068@163.com>
2018-12-14 15:07:00 +0800

    all img

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ import {
 import './App.css';
 import './common.css';
 import imgReact from './image/react.png'
+import imgEgg from './image/egg.png'
+import imgNode from './image/node.png'
+import imgDocker from './image/docker.png'
+import imgJs from './image/js.png'
+import imgKoa from './image/koa.png'
+import imgMarko from './image/marko.png'
+import imgMysql from './image/mysql.png'
+import imgNpm from './image/npm.png'
+import imgPython from './image/python.png'
+import imgRedux from './image/redux.png'
+import imgTravis from './image/travis.png'
+import imgVue from './image/vue.png'
+import imgWebpack from './image/webpack.png'
+
 class App extends Component {
   render() {
     return (
@@ -21,23 +35,30 @@ class App extends Component {
   randomnum( min, max ){
     return Math.floor(Math.random() * ( max-min+1 ) + min)
   }
-  ani () { //动画
+  ani (arr) { //动画
     let H =this.refs.ani.offsetHeight;
     let W =this.refs.ani.offsetWidth;
-    let elehtml = '';
-    //最初生成
-    for ( var i = 0; i<this.randomnum(20,50) ; i ++ ) {
-       let eleW = this.randomnum(50, 100) + 'px';
-       let eletop =  this.randomnum( 100 , H ) - 100 + 'px';
-       let eleleft = this.randomnum( 0 , W) + 'px';
-       elehtml += `<img ref="reactimg" style="width:${eleW} ; position: absolute;left : ${eleleft} ; top : ${eletop}; " src=${imgReact} />`
+    let allimg='';
+    for ( var j = 0 ; j < arr.length ; j++ ){
+      let elehtml = '';
+      //最初生成
+      for ( var i = 0; i<this.randomnum(5,10) ; i ++ ) {
+         let eleW = this.randomnum(20, 30) + 'px';
+         let eletop =  this.randomnum( 30 , H ) - 30 + 'px';
+         let eleleft = this.randomnum( 30 , W) - 30 + 'px';
+         elehtml += `<img ref="reactimg" style="width:${eleW} ; position: absolute;left : ${eleleft} ; top : ${eletop}; " src=${arr[j]} />`
+      }
+      //elehtml
+      allimg += elehtml
     }
-    //
-    this.refs.ani.innerHTML=elehtml;
-    console.log(H,W)
+    this.refs.ani.innerHTML=allimg;
   }
   componentDidMount(){
-     this.ani()
+     let arrImg = [ imgReact, imgEgg, imgNode, imgDocker, imgJs, imgKoa, 
+                   imgMarko, imgMysql, imgNpm, imgPython, imgRedux, imgTravis
+                   , imgVue, imgWebpack
+     ]
+     this.ani(arrImg)
   }
 }
 
