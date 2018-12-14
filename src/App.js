50a069c73e9152d7e811bc3571a4675f28dd70fc
QinZonger <17611573068@163.com>
2018-12-14 14:09:23 +0800

    react

--- src/App.js
+++ src/App.js
@@ -14,14 +14,30 @@ class App extends Component {
     return (
       <div className="App">
        <header></header>
-       <div className="ani"></div>
-       {/* <li><Link to="/index">Home</Link></li>
-       <img className="app-img" src={imgReact} alt="" /> */}
+       <div className="ani" ref="ani"></div>
       </div>
     );
   }
+  randomnum( min, max ){
+    return Math.floor(Math.random() * ( max-min+1 ) + min)
+  }
+  ani () { //动画
+    let H =this.refs.ani.offsetHeight;
+    let W =this.refs.ani.offsetWidth;
+    let elehtml = '';
+    //最初生成
+    for ( var i = 0; i<this.randomnum(20,50) ; i ++ ) {
+       let eleW = this.randomnum(50, 100) + 'px';
+       let eletop =  this.randomnum( 100 , H ) - 100 + 'px';
+       let eleleft = this.randomnum( 0 , W) + 'px';
+       elehtml += `<img ref="reactimg" style="width:${eleW} ; position: absolute;left : ${eleleft} ; top : ${eletop}; " src=${imgReact} />`
+    }
+    //
+    this.refs.ani.innerHTML=elehtml;
+    console.log(H,W)
+  }
   componentDidMount(){
-    
+     this.ani()
   }
 }
 
